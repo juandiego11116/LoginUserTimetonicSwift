7b2a896cb8402155def89e89be67444d6ffad040
juandiego11116@gmail.com <71513912+juandiego11116@users.noreply.github.com>
2024-01-22 20:31:59 -0500

    feat: changed from UITextField to PasswordTextField

--- LoginTimetonicApp/LoginTimetonicApp/View/LogInViewController.swift
+++ LoginTimetonicApp/LoginTimetonicApp/View/LogInViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import PasswordTextField
 
-class ViewController: UIViewController {
+class LogInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
