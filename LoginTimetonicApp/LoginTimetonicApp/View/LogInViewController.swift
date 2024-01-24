09a243cc804b35ec3865bf29315ec358f103b224
juandiego11116@gmail.com <71513912+juandiego11116@users.noreply.github.com>
2024-01-24 18:30:40 -0500

    Feat: added Textfield and Button to

--- LoginTimetonicApp/LoginTimetonicApp/View/LogInViewController.swift
+++ LoginTimetonicApp/LoginTimetonicApp/View/LogInViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 import PasswordTextField
 
 class LogInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: PasswordTextField!
+    @IBAction func loginPressed(_ sender: UIButton){
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
 
 }
 
